4904a202871e9d063e4b056d3c1726efc2df59ff
Husen Umer <husen.umer@kaust.edu.sa>
2025-02-19 16:46:14 +0300

    fixed issues in downloading ncbi datasets

--- download_ncbi_data.py
+++ download_ncbi_data.py
@@ -93,17 +93,16 @@ def download_genome(species, output_dir, max_retries=3, file_types='genome,gtf')
             result = subprocess.run(cmd, shell=True, capture_output=True, text=True, cwd=species_dir)
             if result.returncode == 0:
                 #Unzip downloaded folder that contains the files
-                zip_path =  species_dir / "ncbi_dataset.zip"
+                zip_file =  species_dir / "ncbi_dataset.zip"
                 extract_path = species_dir 
                 try:
-                    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+                    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
                         zip_ref.extractall(extract_path)
                         # Check if download was successful by looking for the data report
                         if dataset_catalog.exists():
                             # Clean up zip file
-                            zip_file.unlink()
                             print(f"Successfully downloaded data for {species}")
-                            zip_path.unlink()
+                            zip_file.unlink()
                             return (species, True)
                 except zipfile.BadZipFile as e:
                     print(f'Error in unzipping {species}. Error: {e}')
@@ -120,36 +119,6 @@ def download_genome(species, output_dir, max_retries=3, file_types='genome,gtf')
         pass
     return (species, False)
 
-def get_species_names(search_term, max_results=100):
-    """
-    Fetches a list of species names from NCBI datasets tool based on a search term.
-    
-    Args:
-        search_term (str): The term to search for in the NCBI datasets tool.
-        max_results (int): Maximum number of results to fetch.
-        
-    Returns:
-        list: A list of species names.
-    """
-    try:
-        cmd = f"datasets summary genome taxon all --assembly-source all --limit {max_results}"
-        result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
-        if result.returncode != 0:
-            print(f"Error fetching species names: {result.stderr}")
-            return []
-        
-        data = json.loads(result.stdout)
-        species_names = []
-        for genome in data['assemblies']:
-            if genome['organism']['name'] not in species_names:
-                species_names.append(genome['organism']['name'])
-        
-        return species_names
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return []
-
 def fetch_ncbi_species(max_species, species_list_file=None, search_term='all', assembly_source='all', reference=True):
     """
     Fetch species names from NCBI datasets tool.
@@ -162,10 +131,9 @@ def fetch_ncbi_species(max_species, species_list_file=None, search_term='all', a
         list: List of species names.
     """
     try:
-        cmd = f"datasets summary genome taxon {search_term} --assembly-source {assembly_source} --limit {max_species} --as-json-lines"
+        cmd = f"datasets summary genome taxon '{search_term}' --assembly-source {assembly_source} --limit {max_species} --as-json-lines"
         if reference: #limit to reference genomes only
             cmd+=' --reference'
-        
         result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
         if result.returncode != 0:
             print(f"Error fetching species names: {result.stderr}")
@@ -175,7 +143,7 @@ def fetch_ncbi_species(max_species, species_list_file=None, search_term='all', a
         
         for genome in data_sets:
             if genome:
-                species_name = json.loads(genome)['organism']['organism_name']
+                species_name = json.loads(genome)['organism']['organism_name'].replace("'", "").replace("[", "").replace("]", "")
                 #species_name = genome['assembly_info']['assembly_name']
                 if species_name not in species_list:
                     species_list.append(species_name)
@@ -183,7 +151,7 @@ def fetch_ncbi_species(max_species, species_list_file=None, search_term='all', a
                     break
     except Exception as e:
         print(f"An error occurred: {e}")
-
+    
     if species_list_file and species_list:
         with open(species_list_file, 'w') as species_list_outfile:
             species_list_outfile.write('\n'.join(list(set(species_list))))
@@ -191,50 +159,36 @@ def fetch_ncbi_species(max_species, species_list_file=None, search_term='all', a
 
 def main():
     parser = argparse.ArgumentParser(description="Download genome and GTF files from NCBI for multiple species")
-    group = parser.add_mutually_exclusive_group(required=True)
+    group = parser.add_mutually_exclusive_group(required=False)
     group.add_argument("-s", "--species", nargs="+", 
                         help="List of species names or taxon IDs")
     group.add_argument("-f", "--file", type=str, 
                         help="File containing species names or taxon IDs (one per line)")
     parser.add_argument("-st", "--search_term", type=str, default='all',
                         help="Search term to identify species name and if it exists")
+    
     parser.add_argument("-ar", "--assembly_source", type=str, default='all',
                         help="Assembly resources to use (either RefSeq or GenBank or all (default))")
-    group.add_argument("-l", "--list_all_species", action='store_true', default=False,
-                        help="List all species available in NCBI")
     parser.add_argument("-r", "--reference_only", action='store_true', default=True,
                         help="All consider reference genomes, default True")
-    parser.add_argument("--species_list_file", default="ncbi_species_list.txt", 
-                        help="File name to save the list of species names when --list_all_species is enabled.")
-    parser.add_argument("-d", "--download_species", action='store_true', default=False,
-                        help="Download species available in NCBI")
     parser.add_argument("-m", "--max_species", type=int, default=1,
                         help="Maximum number of species to download (default: 10k)")
+    
+    parser.add_argument("-d", "--download_species", action='store_true', default=False,
+                        help="Download species available in NCBI")
     parser.add_argument("-a", "--max_attempts", type=int, default=3,
                         help="Maximum number of attempts to retry download (default: 3)")
     parser.add_argument("-ft", "--file_types", nargs="+", default=['genome', 'gtf'],
                         help="""List of assets to download e.g. genome gtf""")
-    parser.add_argument("-e", "--email", type=str, default="bioinfo@kaust.edu.sa",
-                        help="Email address for NCBI Entrez (default: bioinfo@kaust.edu.sa)")
+    
+    parser.add_argument("--species_list_file", default="ncbi_species_list.txt", 
+                        help="File name to save the list of species names.")
     parser.add_argument("-o", "--output", type=str, default="genomes",
                         help="Output directory (default: genomes)")
     parser.add_argument("-p", "--processes", type=int, default=1,
                         help="Number of parallel downloads (default: 1)")
     args = parser.parse_args()
     
-    #list all species
-    if args.list_all_species:
-        species = fetch_ncbi_species(
-                args.max_species, args.species_list_file, args.search_term, args.assembly_source, args.reference_only)
-        print(f"Total species found: {len(species)}")
-    sys.exit(0)
-    if args.search_term:
-        print('Species found:\n', '\n'.join(get_species_names(search_term=args.search_term, max_results=args.max_species)))
-        if not args.download_species:
-            sys.exit(0)
-    
-    if args.max_species>10 and (args.search_term or args.list_all_species):
-        print(f"Retrieving the list of {args.max_species} may take very long time..... It is faster to provide an input file with a long list of species. \nProcess Started... ")
     # Create output directory
     output_dir = Path(args.output)
     output_dir.mkdir(parents=True, exist_ok=True)
@@ -245,22 +199,18 @@ def main():
         species_list = args.species
     elif args.file and os.path.isfile(args.file):
         species_list = [line.strip() for line in open(args.file, 'r').readlines() if line.strip() if line!=""]
-    elif args.search_term and args.download_species:
-        species_list = get_species_names(search_term=args.search_term, max_results=args.max_species)
-    elif args.download_species:
-        species_list = fetch_ncbi_species(
-            fetch_ncbi_species(
-                args.max_species, args.species_list_file, args.search_term, args.assembly_source, args.reference_only)
-        )
+    elif args.search_term or args.download_species:
+        species_list = fetch_ncbi_species(args.max_species, args.species_list_file, args.search_term, args.assembly_source, args.reference_only)
     else:
-        print("Please provide a list of species or a file containing species names.")
-        sys.exit(1)
+        print("Please provide either \na) a list of species\nb) a file containing species names or\n c) a search term.")
+
+    if not species_list:
+        return []
     
     # Process species in parallel
     successful = 0
     failed = 0
     failed_species = []
-    
     results = []
     if args.processes>1:
         with Pool(processes=args.processes) as pool:
@@ -268,7 +218,7 @@ def main():
     else:
         for spcies in species_list:
             results.append(download_genome(spcies, output_dir, args.max_attempts, ','.join(args.file_types)))
-    #print(results)
+    return results
 
 if __name__ == "__main__":
     main()
